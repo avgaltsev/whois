3ff25e1cc90804f2256cb13501e6724aba6824f8
avgaltsev <avgaltsev@gmail.com>
2013-05-06 21:52:58 +0400

    Logger added, TLD recognizer moved to separate module

--- whois/whois.py
+++ whois/whois.py
@@ -1,10 +1,24 @@
-import os, subprocess, re, json
-from bottle import get, post, request, run, default_app
+import subprocess, re, json
 
-@get("/whois/")
-def getIndex():
-   
-   return "Hello"
+from bottle import post, request, run, default_app
+
+import tlds
+
+
+def log(function):
+    
+    def decorator(*args, **kwargs):
+        
+        status = function(*args, **kwargs)
+        
+        if status == "free":
+            file = open("whois.log", "a")
+            file.write(args[0] + "\n")
+            file.close
+        
+        return status
+    
+    return decorator
 
 
 @post("/whois/")
@@ -12,80 +26,22 @@ def postIndex():
     
     match = re.match("[a-zA-Z0-9\-\.]+", request.forms.get("query"))
     
-    query = match.group(0) if match else ""
-    
-    tld = query.split(".")[-1]
+    domain = match.group(0) if match else ""
     
     try:
-        output = subprocess.check_output(["whois", query])
+        whois = subprocess.check_output(["whois", domain])
     
     except Exception as e:
-        output = e.output
-    
-    def com(output):
-        return "free" if re.search("No match for", output) else "occupied"
-    
-    def org(output):
-        return "free" if re.search("NOT FOUND", output) else "occupied"
-    
-    def biz(output):
-        return "free" if re.search("Not found", output) else "occupied"
-    
-    def cat(output):
-        return "free" if re.search("NOT FOUND", output) else "occupied"
-    
-    def coop(output):
-        return "free" if re.search("No domain records were found to match", output) else "occupied"
-    
-    def name(output):
-        return "free" if re.search("No match", output) else "occupied"
-    
-    def ru(output):
-        return "free" if re.search("No entries found for the selected source", output) else "occupied"
-    
-    def default(output):
-        return "wrong"
-    
-    functions = {
-        
-        "com": com,
-        "net": com,
-        
-        "org": org,
-        
-        "aero": org,
-        "asia": org,
-        "biz": biz,
-        "cat": cat,
-        "coop": coop,
-        "info": org,
-        "jobs": com, #tv, cc
-        "mobi": org,
-        "museum": cat,
-        "name": name,
-        "post": org,
-        "pro": org,
-        "tel": biz,
-        "travel": biz,
-        "xxx": org,
-        
-        "me": org,
-        
-        "ru": ru
-        
-    }
-    
-    status = functions.get(tld, default)(output)
-    
-    whois = output
+        whois = e.output
     
     return json.dumps({
-        "status": status,
+        "status": log(tlds.recognize)(domain, whois),
         "whois": whois
     })
 
 
-if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    run(host="0.0.0.0", port=8080)
+
 else:
     application = default_app()
